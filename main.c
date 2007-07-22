790f918b3dc79475201144fe79f15a3062fd9c20
hugo <hugo@21d05eaf-316d-4d00-950b-0ef96bfe89f7>
2007-07-22 13:22:36 +0000

    Splitting into files...
    git-svn-id: svn://127.0.0.1/freespeak/src@18 21d05eaf-316d-4d00-950b-0ef96bfe89f7

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <glib.h>
+
+#include "highlevel.h"
+#include "connection_request.h"
+
+
+int main() {
+	int sockfd;
+	struct sockaddr_in servaddr;
+	
+	struct connection_request connectme = {
+		"Machine Youx",
+		"",
+		"sllm",
+		"Client test!",
+		0
+	};
+    
+    
+	sockfd = connect_to(&connectme, &sockfd, &servaddr);
+	receive(sockfd, &servaddr);
+	return 0;
+}
+
