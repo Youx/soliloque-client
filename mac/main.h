21a25275f94ce6ac52be2a761d4be8d9082ce675
hugo <hugo@21d05eaf-316d-4d00-950b-0ef96bfe89f7>
2008-03-02 13:10:21 +0000

    Added mac folder.
    git-svn-id: svn://127.0.0.1/freespeak/src@63 21d05eaf-316d-4d00-950b-0ef96bfe89f7

---
+++ mac/main.h
@@ -0,0 +1,10 @@
+/*
+ *  main.h
+ *  Freespeak
+ *
+ *  Created by Hugo Camboulive on 01/03/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+void addLineToLogger(char * line);
